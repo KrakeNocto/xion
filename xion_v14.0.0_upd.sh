451a235bd1c1a54ac7e3991959680b1b26eb5c93
KrakeNocto <octo2kraken@gmail.com>
2024-10-24 10:35:33 +0300

    Update xion_v14.0.0_upd.sh

--- xion_v14.0.0_upd.sh
+++ xion_v14.0.0_upd.sh
@@ -8,4 +8,6 @@ rm -rf $HOME/.xiond/data $HOME/.xiond/wasm
 curl https://server-5.itrocket.net/testnet/burnt/burnt_2024-10-24_10469507_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.xiond
 mv $HOME/.xiond/priv_validator_state.json.backup $HOME/.xiond/data/priv_validator_state.json
 
+rm xion_v14.0.0_upd.sh
+
 sudo systemctl restart xiond && sudo journalctl -u xiond -f
