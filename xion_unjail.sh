47377a56396223e10a3689b55b409ae2086a1f14
KrakeNocto <octo2kraken@gmail.com>
2024-10-25 12:30:48 +0300

    Create xion_unjail.sh

---
+++ xion_unjail.sh
@@ -0,0 +1,16 @@
+echo "Sleeping 0 seconds (0 hours)"
+sleep 0
+
+min_time_c=600
+max_time_c=43200
+sleep_time_c=$(shuf -i $min_time_c-$max_time_c -n 1)
+
+echo "Unjail validator after $sleep_time_c seconds"
+sleep $sleep_time_c
+
+export PWD="SorexGroup"
+
+PORT=$(grep -oP '(0\.0\.0\.0|127\.0\.0\.1):\K[0-9]*57' .xiond/config/config.toml)
+echo "$PWD" | /root/go/bin/xiond --node tcp://0.0.0.0:$PORT tx slashing unjail --from wallet -y
+
+rm xion_unjail.sh
